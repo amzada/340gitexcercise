399d27f90003ae3d3106d39605c915efd5b331bd
Ahmed Zada <A.Zada@ahmeds-macbook-pro.local>
2017-10-01 14:34:27 +0100

    Initial Commit

---
+++ 3.40GitExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3.40GitExercise
+//
+//  Created by Ahmed Zada on 01/10/2017.
+//  Copyright © 2017 Ahmed Zada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
